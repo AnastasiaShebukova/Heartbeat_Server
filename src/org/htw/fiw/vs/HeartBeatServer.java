6e4219982afd38bdacca2dd4b6fb506948990c1b
FIXED-TERM Neuber Leana (INST/ESY2) <leana.neuber@bosch-si.com>
2017-12-11 15:20:32 +0100

    neuer Kommentar

--- src/org/htw/fiw/vs/HeartBeatServer.java
+++ src/org/htw/fiw/vs/HeartBeatServer.java
@@ -6,10 +6,7 @@ import java.rmi.RemoteException;
 public class HeartBeatServer implements HeartBeatObserverInterface {
 	public static void main(String[] args) {
 		try {
-			HeartBeat hb = (HeartBeat) Naming.lookup("//localhost/HeartBeat");
-			HeartBeatSubjectInterface hbsubject = (HeartBeatSubjectInterface) Naming.lookup("//localhost/HeartBeatSubj");
-			//SpotifyService ss = (SpotifyService) Naming.lookup("//localhost/SpotifyService");
-			//System.out.println(ss.adaptVolume(hb.getHeartBeat()));
+			HeartBeatSubjectInterface hbsubject = (HeartBeatSubjectInterface) Naming.lookup("//localhost/HeartBeat");
 			HeartBeatServer hbserver = new HeartBeatServer();
 			hbsubject.subscribeObject(hbserver);
 		} catch (Exception e){
@@ -20,6 +17,8 @@ public class HeartBeatServer implements HeartBeatObserverInterface {
 	@Override
 	public void update(Object observable, int heartbeat) throws RemoteException {
 		System.out.println("got heartrate:" + heartbeat);
+		//hier vielleicht dann lookup nach dem spotify service und entsprechende aktionen dort? würde dann aber bei jedem update eben neu
+		//aufgerufen werden.. bzw. gesucht. Oder das auch mit Observer pattern, dass wir dann hier das subject sind? 
 	}
 }
 
