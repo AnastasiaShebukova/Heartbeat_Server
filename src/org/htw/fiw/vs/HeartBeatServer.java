5dec3db7c1aabf2946008f3662b4f88176cd3afa
FIXED-TERM Neuber Leana (INST/ESY2) <leana.neuber@bosch-si.com>
2017-12-11 15:17:34 +0100

    Anpassungen nach einem Tutorial

--- src/org/htw/fiw/vs/HeartBeatServer.java
+++ src/org/htw/fiw/vs/HeartBeatServer.java
@@ -1,16 +1,25 @@
 package src.org.htw.fiw.vs;
 
 import java.rmi.Naming;
+import java.rmi.RemoteException;
 
-public class HeartBeatServer {
+public class HeartBeatServer implements HeartBeatObserverInterface {
 	public static void main(String[] args) {
 		try {
-			HeartBeat hb = (HeartBeat) Naming.lookup("//localhost/HeartBeatService");
-			SpotifyService ss = (SpotifyService) Naming.lookup("//localhost/SpotifyService");
-			System.out.println(ss.adaptVolume(hb.getHeartBeat()));
+			HeartBeat hb = (HeartBeat) Naming.lookup("//localhost/HeartBeat");
+			HeartBeatSubjectInterface hbsubject = (HeartBeatSubjectInterface) Naming.lookup("//localhost/HeartBeatSubj");
+			//SpotifyService ss = (SpotifyService) Naming.lookup("//localhost/SpotifyService");
+			//System.out.println(ss.adaptVolume(hb.getHeartBeat()));
+			HeartBeatServer hbserver = new HeartBeatServer();
+			hbsubject.subscribeObject(hbserver);
 		} catch (Exception e){
 			System.out.println(e);
 		}
+	}
+
+	@Override
+	public void update(Object observable, int heartbeat) throws RemoteException {
+		System.out.println("got heartrate:" + heartbeat);
 	}
 }
 
